2a1906a7e561b944e0e46fcc8eb957aa0724c4c6
Oshoaro Favour <oshoarofavour@gmail.com>
2024-05-17 13:29:36 +0100

    created a route and model folder

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+require('dotenv').config();
+
 const express = require('express')
+const cors = require('cors')
+const loginRoute = require('./routes/loginRoute')
+
+
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Testing the backend')
+app.use(cors())
+app.use(express.json())
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
+
+let db= mongoose.connection;
+db.once('open', function() {
+    console.log("DATABASE CONNECTED")
 })
 
+db.on("error", function (err) {
+    console.error("Error connecting to MongoDB:", err);
+});
+
+
+app.use('/api/login', loginRoute)
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
 
 
