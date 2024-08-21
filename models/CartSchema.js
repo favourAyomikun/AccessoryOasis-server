6f7f09fdebd2d3fbcd3ea73167b00ffc8e36b782
Oshoaro Favour <oshoarofavour@gmail.com>
2024-08-21 20:25:22 +0100

    wrote the route logic for the cart & called it in the index.js

--- models/CartSchema.js
+++ models/CartSchema.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 // created a schema for cart that stores userId and cart items
 const cartSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    items: [{
-        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Accessory' },
-        quantity: { type: Number, required: true }
-      }]
-})
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  items: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Accessory" },
+      quantity: { type: Number, required: true },
+    },
+  ],
+});
 
-const cartModel = mongoose.model('Cart', cartSchema)
+const cartModel = mongoose.model("Cart", cartSchema);
 
 module.exports = cartModel;
