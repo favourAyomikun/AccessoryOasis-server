490e9acf4a8d695d3e38c50964561b96ee4565ba
Oshoaro Favour <oshoarofavour@gmail.com>
2024-08-21 23:48:10 +0100

    done with the register logic

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -1,45 +1,47 @@
-const express = require('express')
-const userModel = require('../models/RegisterSchema')
-const jwt = require('jsonwebtoken');
-const router = express.Router()
+const argon2 = require("argon2");
+const express = require("express");
+const userModel = require("../models/RegisterSchema");
+const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    // destructure user details
-    const { username, email, password } = req.body
+router.post("/register", async (req, res) => {
+  // destructure user details
+  const { username, email, password } = req.body;
 
-    try {
-        // check if user exists
-        let user = await userModel.findOne({ email })
-        if (user) {
-            return res.status(400).json({ message: 'User already exists' })
-        }
-
-        // create new user
-        user = new userModel({
-            username, 
-            email,
-            password
-        })
-
-        // save user to the database
-        await user.save()
-
-        // create jwt payload
-        const payload = { user: { id: user.id } }
-        
-        // sign jwt token
-        jwt.sign(payload,
-            'accessoryoasis_ecommerce',
-            { expiresIn: 360000 },
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token })
-            }
-        )
-    } catch (err) {
-        console.error(err.message)
-        res.status(500).send('Server error')
+  try {
+    // check if user exists
+    let user = await userModel.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
     }
-})
+
+    // hashed password with argon
+    const hashedPassword = await argon2.hash(password, {
+      type: argon2.argon2d,
+    });
+
+    // create new user
+    user = new userModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // save user to the database
+    await user.save();
+
+    // Send response with user data and success message
+    res.status(201).json({
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      message: "Account created successfully",
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
