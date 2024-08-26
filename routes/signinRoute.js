a13c8fff6eddb2804d1c538166e27af4c29c8d34
Oshoaro Favour <oshoarofavour@gmail.com>
2024-08-26 23:11:25 +0100

    added a logic to the sign in route

--- routes/signinRoute.js
+++ routes/signinRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const argon2d  = require("argon2");
+const argon2d = require("argon2");
 const userModel = require("../models/UserSchema");
 const router = express.Router();
 
@@ -31,16 +31,21 @@ router.post("/login", async (req, res) => {
 
     // Generate JWT token for the authenticated user
     const token = generateToken(existingUser);
+    console.log("Generated JWT Token:", token);
 
+    // Decode the token to check the payload directly on the backend
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded Token Payload:", decodedToken);
+    
     // send response with token, user data and success message
     res.status(200).json({
       user: {
         id: existingUser.id,
         username: existingUser.username,
-        email: existingUser.email
+        email: existingUser.email,
       },
       token,
-      message: 'Login successful'
+      message: "Login successful",
     });
     console.log(token);
   } catch (err) {
