4e94c843d24a76530a5857d0cf88447cf1069814
Oshoaro Favour <oshoarofavour@gmail.com>
2024-09-09 14:16:05 +0100

    wrote the lofic for the otp schema

---
+++ models/OtpSchema.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require("mongoose");
+
+const otpSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    otp: {
+        type: String,
+        required: true
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now, expires: 300 
+    }, // OTP expires in 5 minutes
+})
--- routes/verifyEmailRoute.js
+++ routes/verifyEmailRoute.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const crypto = require('crypto')
 
 
 router.post('/send-otp', async (req, res) => {
     const { email } = req.body;
 
     // generate a 6-digit otp
+    const otp = crypto.randomInt(100000, 999999)
+
+    // save otp & email in database
+    const hashedOtp = crypto.createHash('sha256').update(otp.toString()).digest('hex')
+    await 
 })
 
 router.post('/verify-otp', async (req, res) => {
